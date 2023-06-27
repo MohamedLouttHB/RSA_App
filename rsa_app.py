12ac70fd9510846559c29a7201a8e71ba31a1b2a
MohamedLouttHB <m.loutt99@gmail.com>
2023-06-28 00:14:53 +0100

    Initial commit

---
+++ rsa_app.py
@@ -0,0 +1,72 @@
+import random
+import sympy
+import math
+
+# Fonction pour générer un nombre premier aléatoire
+def generer_nombre_premier():
+    while True:
+        nombre = random.randint(2, 1000)  # Choisissez une plage appropriée pour vos besoins
+        if sympy.isprime(nombre):
+            return nombre
+
+# Génération des deux nombres premiers aléatoires p et q
+p = generer_nombre_premier()
+q = generer_nombre_premier()
+
+
+# calcul n et Phi
+n = p * q
+phi = (p-1) * (q-1)
+
+# cle prive e :
+def generer_cle_publique():
+    while True:
+        e = random.randint(2, 1000)  # Choisissez une plage appropriée pour vos besoins
+        if math.gcd(e, phi) == 1:
+            return e
+
+e = generer_cle_publique()
+d = sympy.mod_inverse(e, phi)
+
+
+cle_prive, cle_publique = (n,e), (n,d)
+
+msg = "Hello word!"
+
+message= [ord(ch) for ch in msg]
+print(message)
+
+cipher_text = [pow(m,e,n) for m in message ]
+
+decryt_msg = [pow(c,d,n) for c in cipher_text ]
+clear_text = "".join(chr(ch) for ch in decryt_msg)
+print(cipher_text)
+print(clear_text)
+
+#Affichage des infos
+
+#print("n:", n)
+#print("Phi  :", phi)
+#Affichage des n et phi
+#print("n:", n)
+#print("Phi  :", phi)
+#print("e:", e)
+#print("d:", d)
+
+'''   
+   mp = generer_nombre_premier()
+   mq = generer_nombre_premier()
+
+   # n et Phi
+   mn = mp * mq
+   mphi = (mp - 1) * (mq - 1)
+
+   me = generer_cle_publique()
+   md = sympy.mod_inverse(me, mphi)
+
+    if not (sympy.isprime(p_input)):
+        st.error("p doit étre un nombre premier !")
+    if not (sympy.isprime(q_input)):
+        st.error("q doit étre un nombre premier !")
+
+   '''
---
+++ rsa_streamlit.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import math,sympy
+from rsa_app import generer_nombre_premier, generer_cle_publique
+from PIL import Image
+
+
+st.set_page_config(
+    page_title="RSA Algorithm App",
+    page_icon="🔐",
+
+)
+
+
+
+st.title(":blue[RSA Algorithm]")
+
+#Side bar
+with st.sidebar:
+    st.title(":blue[Fonctionnement RSA]")
+    st.write("1 - Choix de 2 nombres premiers :red[_p_] et :red[_q_] ")
+    st.write("2 - Calcul de : :red[_n = p * q_]")
+    st.write("3 - Calcul de :red[_ϕ(n) = (p-1) * (q-1)_]")
+    st.write("4 - Choix du cle public :red[_e_] tq : :red[_pgcd(e,ϕ(n)) = 1_]")
+    st.write("5 - Calcul du clé privé :red[_d_] tq : :red[_e.d ≡ 1 mod n_]")
+    st.write("6 - Crypter le message avec le clé public du destinataire")
+    st.write("7 - Decrypter le message avec le clé privé du destinataire")
+    image = Image.open('rsa.jpeg')
+    st.image(image, caption=' Guide RSA')
+    st.write('**_:blue[Made by] :red[Mohamed Loutt Horma Babana]_**')
+
+gen_choix = st.radio("**:red[Comment voulez vous genérer les clefs ?]**",('Automatiquement','Manuellement'), horizontal=True)
+
+if (gen_choix == 'Manuellement') :
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        p_input = st.number_input('Saisir un nombre premier p',step=1)
+
+    with col2 :
+        q_input = st.number_input('Saisir un nombre premier q',step=1)
+    with col3 :
+        e_input = st.number_input('Saisir un cle publique e',step=1)
+
+
+    if(sympy.isprime(p_input) and sympy.isprime(q_input)) :
+            mn = p_input * q_input
+            mphi = (p_input - 1)*(q_input -1)
+            if math.gcd(e_input, mphi) == 1:
+                md = sympy.mod_inverse(e_input, mphi)
+            else:
+                st.error("e doit etre premier avec ϕ(n)")
+    else :
+        st.error("p et q doit etre premier")
+
+
+    txtm = st.text_area('Entrer un message')
+    txt_msgm =  [ord(ch) for ch in txtm]
+
+    s_cipher_textm = [pow(sm,e_input,mn) for sm in txt_msgm ]
+
+    s_decrypt_textm = [pow(sm,md,mn) for sm in s_cipher_textm ]
+    s_clear_textm = "".join(chr(ch) for ch in s_decrypt_textm)
+
+
+    col1, col2 = st.columns(2)
+    with col1:
+        encrypt = st.button('🔒 Chiffré')
+
+    with col2:
+        decrypt = st.button('🔓 Déchiffré')
+
+    if(encrypt):
+        st.error(s_cipher_textm)
+
+    if(decrypt):
+        st.success(s_clear_textm)
+
+
+
+if (gen_choix == 'Automatiquement') :
+
+    # Génération des deux nombres premiers aléatoires p et q
+    sp = generer_nombre_premier()
+    sq = generer_nombre_premier()
+
+    # n et Phi
+    sn = sp * sq
+    sphi = (sp-1) * (sq-1)
+
+    se = generer_cle_publique()
+    sd = sympy.mod_inverse(se, sphi)
+
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    col1.write(f"p = {sp}")
+    col2.write(f"q = {sq}")
+    col3.write(f"n = {sn}")
+    col4.write(f"ϕ(n) = {sphi}")
+    col5.write(f"clé publique = {se}")
+    col6.write(f"clé privé = {sd}")
+
+
+    txt = st.text_area('Entrer un message')
+    txt_msg =  [ord(ch) for ch in txt]
+
+    s_cipher_text = [pow(sm,se,sn) for sm in txt_msg ]
+
+    s_decrypt_text = [pow(sm,sd,sn) for sm in s_cipher_text ]
+    s_clear_text = "".join(chr(ch) for ch in s_decrypt_text)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        chiffre = st.button('🔒 Chiffré')
+
+    with col2:
+        dechiffre = st.button('🔓 Déchiffré')
+
+    if(chiffre):
+        st.error(s_cipher_text)
+
+    if(dechiffre):
+        st.success(s_clear_text)
+
+
+
+
+
